0240695e7512c3dcbc758b6bc2a38838e4e13052
BraithwaiteCodes <joshua@thebraithwaitefamily.com>
2023-09-01 11:46:50 +1200

    configuring the main files, including html

--- app.py
+++ app.py
@@ -8,3 +8,16 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 # Configure application
 app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Use CS50 library for configuring the database
+db = SQL("sqlite:///squash.db")
+
+
+@app.route("/")
+def home():
+    return "Hello, Flask!"
