b83e05516b6b0b3f68160e9d96c6a6d7c16c538f
BraithwaiteCodes <joshua@thebraithwaitefamily.com>
2023-09-02 11:39:13 +1200

    Login functionality and layout. Fixed routing

--- app.py
+++ app.py
@@ -18,6 +18,54 @@ Session(app)
 db = SQL("sqlite:///squash.db")
 
 
-@app.route("/")
-def home():
-    return "Hello, Flask!"
+# Ensure reponses are not cached
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (by submitting form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            flash("Must provide a username")
+            return redirect("/login", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            flash("Must provide a password")
+            return redirect("/login", 403)
+
+        # Query database for unique username
+        users = db.execute(
+            "SELECT * FROM usersInfo WHERE username = ?", request.form.get(
+                "username")
+        )
+
+        # Ensure username exists and password is correct
+        if len(users) != 1 or not check_password_hash(
+            users[0]["hash"], request.form.get("password")
+        ):
+            flash("Invalid username and/or password")
+            return redirect("/login", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = users[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET
+    else:
+        return render_template("/login.html")
